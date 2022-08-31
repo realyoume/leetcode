ee6c1feb9c4af94cc4947e531d9ddf4e682c8dd3
realyoume <realyoume@foxmail.com>
2022-08-31 18:37:58 +0800

    17. 电话号码的字母组合

---
+++ 17. 电话号码的字母组合/Solution.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class Solution {
+    public List<String> letterCombinations(String digits) {
+        List<String> combinations = new ArrayList<String>();
+        if (digits.length() == 0) {
+            return combinations;
+        }
+        Map<Character, String> phoneMap = new HashMap<Character, String>() {{
+            put('2', "abc");
+            put('3', "def");
+            put('4', "ghi");
+            put('5', "jkl");
+            put('6', "mno");
+            put('7', "pqrs");
+            put('8', "tuv");
+            put('9', "wxyz");
+        }};
+        backtrack(combinations, phoneMap, digits, 0, new StringBuffer());
+        return combinations;
+    }
+
+    public void backtrack(List<String> combinations, Map<Character, String> phoneMap, String digits, int index, StringBuffer combination) {
+        if (index == digits.length()) {
+            combinations.add(combination.toString());
+        } else {
+            char digit = digits.charAt(index);
+            String letters = phoneMap.get(digit);
+            int lettersCount = letters.length();
+            for (int i = 0; i < lettersCount; i++) {
+                combination.append(letters.charAt(i));
+                backtrack(combinations, phoneMap, digits, index + 1, combination);
+                combination.deleteCharAt(index);
+            }
+        }
+    }
+}
+
+/*
+    自己写的时间空间复杂度都很高
+    用哈希映射存储了数字对应的字母，方便查询
+    用回溯法来找到所有的电话，之前没有这样写过，这样就找到了所有的可能情况
+    有点类似树的深度优先遍历，先往前走到底，然后退一步，走另外可能的方向，走到头再往后退，一直走完所有可能的
+ */
