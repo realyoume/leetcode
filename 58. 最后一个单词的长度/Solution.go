ec5a5dfb5b51c293b1cfac0144057e778a8e8566
realyoume <realyoume@foxmail.com>
2022-09-25 21:57:08 +0800

    263. 丑数

--- 58. 最后一个单词的长度/Solution.go
+++ 58. 最后一个单词的长度/Solution.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"strings"
 )
 
 func main() {
-
+	arr := []int{1, 2, 3}
+	arr = append(arr[:2], append([]int{4}, arr[2:]...)...)
+	fmt.Println(arr)
 }
 
 func lengthOfLastWord(s string) int {
